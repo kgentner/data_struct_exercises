980f34b347c076f1e5fb94dfde1e6be21668e8fc
kgentner <gentnerkarl@gmail.com>
2014-11-06 19:34:36 -0800

    added test for unbalanced paren

--- test/ds_tests.js
+++ test/ds_tests.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var expect = require('chai').expect;
+var paren = require('../lib/findUnbalancedParen');
 var pez = require('../lib/removeYellowPez');
+
+describe('Find Unbalanced Parentheses', function() {
+  it('gets an accurate output', function() {
+    expect(paren('3+4*6(4+8')).to.eql(5);
+  });
+});
 
 describe('Remove Yellow Pez', function() {
   it('gets an accurate output', function() {
