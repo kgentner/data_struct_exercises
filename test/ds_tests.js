0756803c2da9850c7bacd87487ffbefd1c2de0bd
kgentner <gentnerkarl@gmail.com>
2014-11-06 19:50:56 -0800

    added infix to postfix test

--- test/ds_tests.js
+++ test/ds_tests.js
@@ -2,7 +2,10 @@
 
 var expect = require('chai').expect;
 var paren = require('../lib/findUnbalancedParen');
+var fixer = require('../lib/infixToPostfix');
+// var evalPost = require('../lib/postfixEval');
 var pez = require('../lib/removeYellowPez');
+// var pal = require('../lib/isPalindrome');
 
 describe('Find Unbalanced Parentheses', function() {
   it('gets an accurate output', function() {
@@ -10,8 +13,26 @@ describe('Find Unbalanced Parentheses', function() {
   });
 });
 
+describe('Infix to Postfix Converter', function() {
+  it('gets an accurate output', function() {
+    expect(fixer('2*(3+4*5)+6')).to.eql('2345*+*6+');
+  });
+});
+
+// describe('Postfix Evaluator', function() {
+//   it('gets an accurate output', function() {
+//     expect(evalPost()).to.eql();
+//   });
+// });
+
 describe('Remove Yellow Pez', function() {
   it('gets an accurate output', function() {
     expect(pez(['r', 'y', 'w', 'r', 'r'])).to.eql(['r', 'w', 'r', 'r']);
   });
 });
+
+// describe('Palindrome Checker', function() {
+//   it('gets an accurate output', function() {
+//     expect(pal()).to.eql();
+//   });
+// });
