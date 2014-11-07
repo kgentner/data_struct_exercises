42be4bb0aca5b1c85adc13c94ade02504193a3d4
kgentner <gentnerkarl@gmail.com>
2014-11-06 19:25:03 -0800

    removed eval evil from pez

--- test/ds_tests.js
+++ test/ds_tests.js
@@ -3,7 +3,7 @@
 var expect = require('chai').expect;
 var pez = require('../lib/removeYellowPez');
 
-describe('No Yellow Pez', function() {
+describe('Remove Yellow Pez', function() {
   it('gets an accurate output', function() {
     expect(pez(['r', 'y', 'w', 'r', 'r'])).to.eql(['r', 'w', 'r', 'r']);
   });
